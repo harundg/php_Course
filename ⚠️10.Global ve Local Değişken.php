9512151d09a1b62d47e53133938d87cf57427c31
harunnn035 <harundag0101@gmail.com>
2025-01-15 15:00:55 +0300

    Update ⚠️10.Global ve Local Değişken.php

--- ⚠️10.Global ve Local Değişken.php
+++ ⚠️10.Global ve Local Değişken.php
@@ -1,16 +1,32 @@
 <?php
-$a= 10;
-echo"global sayı = ". $a . "<br/>";
+// Global bir değişken olan $a'ya 10 değerini atıyoruz
+$a = 10;
 
-goster();   
+// Global değişkenin değerini ekrana yazdırıyoruz
+echo "global sayı(Fonksiyon Dışında) = " . $a . "<br/>";
+
+// goster adında bir fonksiyon çağırıyoruz
+
+
+// Global ve local değişkenleri tanımlamak ve kullanmak için bir fonksiyon oluşturuyoruz
 function goster(){
-    $b=20;
-    echo "fun içinde global sayı=". $GLOBALS['a'] ."<br/>";
-    echo "yerel sayı=". $b . "<br/>";
+    // Yerel bir değişken olan $b'ye 20 değerini atıyoruz
+    $b = 20;
+
+    // $GLOBALS dizisi yardımıyla global değişken $a'nın değerini ekrana yazdırıyoruz
+    echo "fun içinde global sayı = " . $GLOBALS['a'] . "<br/>";
+
+    // Fonksiyon içinde tanımlanan yerel değişken $b'nin değerini ekrana yazdırıyoruz
+    echo "<br/>"."yerel sayı(fonksiyonun içinde) = " . $b . "<br/>";
+    return $b;
 }
-echo "yerel sayi =  ". $b . "<br/>";
+
+// Fonksiyon dışında $b değişkenine erişmeye çalışıyoruz
+// Ancak bu bir hata üretecektir, çünkü $b sadece goster fonksiyonu içinde tanımlıdır
+echo ">>>>Fonksiyonun Dışında Yerel Sayı"."$b"."</br>"."</br>";
 
 
-//CALISILACAK
 
-?>a
+$sonuc = goster(); // Fonksiyondan dönen değeri alıyoruz
+echo "Fonksiyon dışında yerel sayı kullanılıyor.(return komutu sayesinde): " . $sonuc . "<br/>";
+
